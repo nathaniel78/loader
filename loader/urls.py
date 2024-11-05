8185ef613c7625e3fb4e19e90f8aea0f173853fe
nathaniel <nathaniel.jose@ifam.edu.br>
2024-11-04 22:45:31 -0400

    Corrigindo settings, docker-compose e urls

--- loader/urls.py
+++ loader/urls.py
@@ -4,6 +4,6 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='/home/', permanent=True)),
-    path('', include('app.urls')),  # Define o app como padrão
+    path('', RedirectView.as_view(url='/upload/', permanent=True)),
+    path('', include('app.urls')), 
 ]
